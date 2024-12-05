8f29f09358f204b4597dfca2a6ca4f6ec23795d2
Newtbytes <186638664+Newtbytes@users.noreply.github.com>
2024-12-05 08:54:45 -0800

    Implement registering as gymnasium env

--- celeste/__init__.py
+++ celeste/__init__.py
@@ -1,1 +1,7 @@
-from .env import CelesteEnv
+from .celeste_env import CelesteEnv
+
+from gymnasium.envs.registration import register
+
+__all__ = [CelesteEnv]
+
+register(id="CelesteClassic-v0", entry_point="celeste_env:CelesteEnv")
